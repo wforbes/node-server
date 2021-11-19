cb3cfd0f46ead0737bf66ed5a7f7920f6e435af9
Will Forbes <wforbes87@gmail.com>
2021-11-18 23:01:48 -0800

    Add blog template files, install nodemon
    - Remove pages directory, clear out app.get response calls that reference its pages from server index
    - Add Startboostrap 'clean blog' template files to public directory
    - Install nodemon with --save-dev

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-	console.log('Listening on port 3000');
-});
-
-const pagesDir = __dirname + '/pages/';
-
-app.get(['/', '/index'], (req, res) => {
-	res.sendFile(path.resolve(pagesDir, 'index.html'));
-});
-
-app.get('/about', (req, res) => {
-	res.sendFile(path.resolve(pagesDir, 'about.html'));
-});
-
-app.get('/contact', (req, res) => {
-	res.sendFile(path.resolve(pagesDir, 'contact.html'));
+app.listen(4000, () => {
+	console.log('Listening on port 4000');
 });
