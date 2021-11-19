d153b39dba9c01b3ad87f0e3dd13f4dbfbbfac79
Will Forbes <wforbes87@gmail.com>
2021-11-18 23:17:45 -0800

    Specify routes, move pages, refactor links
    - add 'start' script property in package.json to start the app with nodemon by running 'npm start'
    - move pages from 'public' to 'pages' directory
    - specify routes in server index file with a loop on routes array
    - change the links in the template page files from html file names to names that match the routes (nav and brand links)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
@@ -6,3 +7,30 @@ app.use(express.static('public'));
 app.listen(4000, () => {
 	console.log('Listening on port 4000');
 });
+
+const pagesDir = __dirname + '/pages/';
+
+const routes = [
+	{
+		path: '/',
+		file: 'index.html'
+	},
+	{
+		path: '/about',
+		file: 'about.html'
+	},
+	{
+		path: '/contact',
+		file: 'contact.html'
+	},
+	{
+		path: '/post',
+		file: 'post.html'
+	}
+];
+
+for (let route of routes) {
+	app.get(route.path, (req, res) => {
+		res.sendFile(path.resolve(pagesDir, route.file));
+	});
+}
