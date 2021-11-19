8f302503f44ea89574da4a8d77b21d455c862c1c
Will Forbes <wforbes87@gmail.com>
2021-11-18 21:36:41 -0800

    add handler for static files (css, js)
    - add full html markup to pages
    - add references to css and js files in each page head
    - add express use call for the public directory for static css and js asset files

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static('public'));
+
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
 });
